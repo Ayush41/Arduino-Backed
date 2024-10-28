8242312f1be2b5f4247e9b367d67c39414909667
ayush41 <anonymousking41681@gmail.com>
2024-10-28 23:00:54 +0530

    sensor open/close

---
+++ sensor/sensor.ino
@@ -0,0 +1,30 @@
+#define IR_SENSOR_PIN 2  // Change this to the appropriate pin number
+#define THRESHOLD 500    // Adjust this value based on your IR sensor's characteristics
+
+unsigned long lastStateChangeTime = 0;
+bool isClose = false;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(IR_SENSOR_PIN, INPUT);
+}
+
+void loop() {
+  int sensorValue = analogRead(IR_SENSOR_PIN);
+  bool currentState = (sensorValue > THRESHOLD);
+
+  if (currentState != isClose) {
+    unsigned long currentTime = millis();
+    unsigned long duration = currentTime - lastStateChangeTime;
+
+    // Send the state change information
+    Serial.print(currentState ? "CLOSE" : "FAR");
+    Serial.print(",");
+    Serial.println(duration);
+
+    isClose = currentState;
+    lastStateChangeTime = currentTime;
+  }
+
+  delay(100);  // Adjust this delay as needed
+}
